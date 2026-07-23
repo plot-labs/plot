72a10586427a410babe1ca7872297a48b48d366d
qyinm <80995345+qyinm@users.noreply.github.com>
2026-07-23 21:26:32 +0900

    Prune dead code (#14)

--- apps/api/src/main/kotlin/com/plot/api/source/SourceImportRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.plot.api.source
-
-import java.util.UUID
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface SourceImportRepository : JpaRepository<SourceImport, UUID> {
-	fun findByWorkspaceIdAndId(workspaceId: UUID, id: UUID): SourceImport?
-	fun findAllByWorkspaceIdOrderByCreatedAtDesc(workspaceId: UUID): List<SourceImport>
-}
